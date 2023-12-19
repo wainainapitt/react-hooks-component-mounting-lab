7655743fde5d96b0db50cce305986f1579256dec
peter <pngugi.wainaina@gmail.com>
2023-12-20 01:14:48 +0300

    lab done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import Timer from './Timer'
-
 class App extends Component {
-
   //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
@@ -12,51 +9,40 @@ class App extends Component {
 
   //Your code here:
 
-
-
-
-
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
 
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
   render() {
-
     return (
       <div className="App">
         <h1>MultiTimer</h1>
         <button onClick={this.handleAddTimer}>Add New Timer</button>
-
         <div className="TimerGrid">
           {this.renderTimers()}
         </div>
-
       </div>
     );
   }
-
   // returns array of components written in JSX, mapped from this.state.timerIDs
   renderTimers = () => this.state.timerIDs.map(id => {
     return <Timer key={id} id={id} removeTimer={this.removeTimer} />
   })
-
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
       timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
     }))
   }
-
   // removeTimer updates state, removing any timer that matches the provided author
   removeTimer = id => {
     this.setState(prevState => ({
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
     }))
   }
-
-
 }
-
 export default App;
--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 class Timer extends Component {
   state = {
     time: 0,
@@ -7,6 +6,15 @@ class Timer extends Component {
   };
 
   // add your code here
+
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
@@ -19,22 +27,18 @@ class Timer extends Component {
       </section>
     );
   }
-
   //clock functions
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
     }));
   };
-
   stopClock = () => {
     clearInterval(this.interval);
   };
-
   // for the 'x' button,
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
 }
-
 export default Timer;
